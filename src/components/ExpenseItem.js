2b0b2f24e1b0addaa7775ed7b22db04d94529124
AleenaKh <135075216+AleenaKh@users.noreply.github.com>
2023-06-21 00:12:01 -0700

    Update ExpenseItem.js

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,8 +1,25 @@
 import React, { useContext } from 'react';
+import ExpenseItem from './ExpenseItem';
 import { AppContext } from '../context/AppContext';
-import { FaTimesCircle } from 'react-icons/fa';
-
-const ExpenseItem = (props) => {
+const ExpenseList = () => {
+    const { expenses } = useContext(AppContext);
+    return (
+        <table className='table'>
+              <thead className="thead-light">
+            <tr>
+              <th scope="col">Items</th>
+              <th scope="col">Quantity</th>
+              <th scope="col">Unit Price</th>
+              <th scope="col">Items Price</th>
+              <th scope="col">Remove</th>
+            </tr>
+          </thead>
+            <tbody>
+            {expenses.map((expense) => (
+                <ExpenseItem id={expense.id} key={expense.id} name={expense.name} quantity={expense.quantity} unitprice={expense.unitprice} />
+            ))}
+            </tbody>
+        </table>
+    );
 };
-
-export default ExpenseItem;
+export default ExpenseList;
