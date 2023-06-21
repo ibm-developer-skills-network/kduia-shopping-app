7dfb9274a70b54f9ad3399f03b0542eb8534489d
AleenaKh <135075216+AleenaKh@users.noreply.github.com>
2023-06-21 00:14:56 -0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,16 +1,14 @@
 import React from 'react';
-import 'bootstrap/dist/css/bootstrap.min.css'; 
+import 'bootstrap/dist/css/bootstrap.min.css'; 
 import { AppProvider } from './context/AppContext';
 import CartValue from './components/CartValue';
 import ExpenseList from './components/ExpenseList';
 import ItemSelected from './components/ItemSelected';
 import Location from './components/Location';
-
 function App() {
   return (
     <div className="App">
     </div>
   );
 }
-
 export default App;
