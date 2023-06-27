cb99b1969179456a597f5dcb73b1202644857e1b
Eduardo Raygoza <edward12raygoza@gmail.com>
2023-06-26 23:44:48 -0700

    added components

--- src/App.js
+++ src/App.js
@@ -6,11 +6,33 @@ import ExpenseList from './components/ExpenseList';
 import ItemSelected from './components/ItemSelected';
 import Location from './components/Location';
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-    </div>
+      <AppProvider>
+          <div className='container'>
+              <h1 className='mt-3'>Shopping App</h1>
+              <div className='row mt-3'> 
+                  <div className='col-sm'>
+                      <CartValue />
+                  </div>
+                  <div className='col-sm'>
+                      <Location />
+                  </div>
+              </div>
+              <h3 className='mt-3'>Shopping Cart</h3>
+              <div className='row '>
+                  <div className='col-sm'>
+                      <ExpenseList />
+                  </div>
+              </div>
+              <h3 className='mt-3'>Add Items</h3>
+              <div className='row mt-3'>
+                  <div className='col-sm'>
+                      <ItemSelected/>
+                  </div>
+              </div>
+          </div>
+      </AppProvider>
   );
-}
-
+};
 export default App;
