2a82dbf8a7da0273f462063eb31e236883677d59
stierney0505 <stierney0505@gmail.com>
2023-08-30 23:52:15 -0400

    Completed Practice Lab

--- src/App.js
+++ src/App.js
@@ -1,16 +1,36 @@
 import React from 'react';
-import 'bootstrap/dist/css/bootstrap.min.css'; 
+import 'bootstrap/dist/css/bootstrap.min.css';
 import { AppProvider } from './context/AppContext';
 import CartValue from './components/CartValue';
 import ExpenseList from './components/ExpenseList';
 import ItemSelected from './components/ItemSelected';
 import Location from './components/Location';
+import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-    </div>
-  );
+    return (
+        <AppProvider>
+            <div class="App">
+                <div class="item">
+                    <h1>Shopping App</h1>
+                    <div class="columns-container" id="spacing">
+                        <div class="columns"><CartValue></CartValue></div>
+                        <div class="columns"><Location></Location></div>
+                    </div>
+                </div>
+                <div class="item">
+                    <h1>Shopping Cart</h1>
+                    <div id="spacing">
+                        <ExpenseList></ExpenseList>
+                    </div>
+                </div>
+                <div class="item">
+                    <h1>Add Items</h1>
+                    <div><ItemSelected></ItemSelected></div>
+                </div>
+            </div>
+        </AppProvider>
+    );
 }
 
 export default App;
