2988642dfb1322d6b8ddae949fa04dabf3c03bb5
Sergio Pinto <serrique@icloud.com>
2023-10-01 11:53:23 -0400

    improving shopping cart app adding lists and add USA and Brazil locations
    Signed-off-by: Sergio Pinto <serrique@icloud.com>

--- src/components/Location.js
+++ src/components/Location.js
@@ -2,6 +2,29 @@ import React, { useContext } from 'react';
 import { AppContext } from '../context/AppContext';
 
 const Location = () => {
+  const {dispatch } = useContext(AppContext);
+
+    const changeLocation = (val)=>{
+            dispatch({
+                type: 'CHG_LOCATION',
+                payload: val,
+            })
+    }
+    
+
+  return (
+        <div className='alert alert-secondary'> Location {
+      <select name="Location" id="Location" onChange={event=>changeLocation(event.target.value)}>
+        <option value="£">Uk(£)</option>
+        <option value="₹">India(₹)</option>
+        <option value="€">Europe(€)</option>
+        <option value="CAD">Canada(CAD)</option>
+        <option value="BRL">Brazil(BRL)</option>
+        <option value="USD">USA(USD)</option>
+      </select>	
+      }	
+    </div>
+    );
 };
 
 export default Location;
