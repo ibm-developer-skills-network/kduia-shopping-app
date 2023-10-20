ba314d3962c8d5c4bd567bc3e7f7213fecb4d193
Emack2023 <147569908+Emack2023@users.noreply.github.com>
2023-10-20 16:09:32 -0400

    Update ExpenseList.js

--- src/components/ExpenseList.js
+++ src/components/ExpenseList.js
@@ -1,7 +1,27 @@
-import React, { useContext } from 'react';
-import { AppContext } from '../context/AppContext';
+const ExpenseItem = (props) => {
+    const { dispatch, Location} = useContext(AppContext);
 
-const ExpenseList = () => {
+    const handleDeleteItem = () => {
+        const item = {
+            name: props.name,
+        };
+
+        dispatch({
+            type: 'DELETE_ITEM',
+            payload: item,
+        });
+    };
+
+
+    return (
+        <tr>
+        <td>{props.name}</td>
+        <td>{props.quantity}</td>
+        <td>{Location}{parseInt(props.unitprice)}</td>
+        <td>{Location}{parseInt(props.quantity)*parseInt(props.unitprice)}</td>
+        <td><FaTimesCircle size='2.2em' color="red" onClick={handleDeleteItem}></FaTimesCircle></td>
+        </tr>
+    );
 };
 
-export default ExpenseList;
+export default ExpenseItem;
