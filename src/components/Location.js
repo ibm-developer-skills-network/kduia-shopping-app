f617515f7f245314a13558f6caa583012ad8384a
whytemors <snrwhyte91@yahoo.com>
2023-10-25 13:42:24 -0400

    final project

--- src/components/Location.js
+++ src/components/Location.js
@@ -2,6 +2,27 @@ import React, { useContext } from 'react';
 import { AppContext } from '../context/AppContext';
 
 const Location = () => {
+  const {dispatch } = useContext(AppContext);
+
+    const changeLocation = (val)=>{
+            dispatch({
+                type: 'CHG_LOCATION',
+                payload: val,
+            })
+    }
+    
+
+  return (
+        <div className='alert alert-secondary'> Currency {
+      <select name="Location" id="Location" onChange={event=>changeLocation(event.target.value)}>
+        <option value="£">$ Dollar</option>
+        <option value="₹">£ Pound</option>
+        <option value="€">€ Euro</option>
+        <option value="CAD">₹ Rupee</option>
+      </select>	
+      }	
+    </div>
+    );
 };
 
 export default Location;
