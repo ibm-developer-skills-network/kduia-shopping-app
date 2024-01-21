591b812dc10247cde1ac49e0093caa399da714f4
Andy Cunningham <shadnezzarcham@gmail.com>
2024-01-21 16:27:40 -0500

    change files as led

--- src/App.js
+++ src/App.js
@@ -8,8 +8,32 @@ import Location from './components/Location';
 
 function App() {
   return (
-    <div className="App">
-    </div>
+    <AppProvider>
+        <div className='container'>
+            <h1 className='mt-3'>Shopping App</h1>
+            <div className='row mt-3'>
+                <div className='col-sm'>
+                    <CartValue />
+                </div>
+                <div className='col-sm'>
+                    <Location />
+                </div>
+            </div>
+        </div>
+        <h3 className='mt-3'>Shopping Cart</h3>
+        <div className='row'>
+            <div className='com-sm'>
+                <ExpenseList />
+            </div>
+        </div>
+        <h3 className='mt-3'>Add Items</h3>
+        <div className='row mt-3'>
+            <div className='col-sm'>
+                <ItemSelected />
+            </div>
+        </div>
+
+    </AppProvider>
   );
 }
 
