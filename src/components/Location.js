bdfcbc92e99794d89f6007aa7eee18f29268c770
Andrii Bondaruk <dubno0905@gmail.com>
2024-02-23 10:06:36 -0500

    shopping app

--- src/components/Location.js
+++ src/components/Location.js
@@ -1,7 +1,28 @@
-import React, { useContext } from 'react';
+import React, { useContext } from 'react'
 import { AppContext } from '../context/AppContext';
 
 const Location = () => {
+    const { dispatch } = useContext(AppContext);
+
+    const changeLocation = (val) => {
+        dispatch({
+            type: 'CHG_LOCATION',
+            payload: val,
+        })
+    }
+
+    return (
+        <div className='alert alert-secondary'>
+            Loaction {
+                <select name="Location" id="Location" onChange={event=>changeLocation(event.target.value)}>
+                    <option value=''>UK</option>
+                    <option value='₹'>India</option>
+                    <option value='€'>Europe</option>
+                    <option value='CAD'>Canada</option>
+                </select>
+            }
+        </div>
+    );
 };
 
 export default Location;
